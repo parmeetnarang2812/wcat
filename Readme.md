1a99f8fa971992fcfc630846d65108d665344cb6
parmeetnarang2812 <56155933+parmeetnarang2812@users.noreply.github.com>
2021-06-12 20:38:49 +0530

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,16 @@
+##Wcat 🚀🚀
+It is used to display or make a copy content of one or more files in the terminal
+
+##Commands:
+💯1- wcat filepath => displays content of the file in the terminal ✔
+💯2- wcat filepath1 filepath2 filepath3... => displays content of all files in the terminal(contactinated form) in the given order. ✔
+💯line breaks into a singular line break ✔
+💯4- wcat -n filepath => give numbering to all the lines ✔
+💯5- wcat -b filepath => give numbering to non-empty lines ✔
+💯6- wcat filepath > filename2path => put all the content of filename into filename2 by overriding and also creates filename2 if it doesn't exist. ✔
+💯7- wcat filename2path >> filename2path => append all the content of filename into filename2✔
+💯8- node wcat -s filename > filename2 =>get the file content of filename remove large spaces and save the output in filename2 ✔ We can mix and match the options.
+
+💯Edge cases:
+1- If file entered is not found then it gives file does not exist error.
+2- -n and -b are 2 options available together then command should give you an error
---
+++ wcat.js
@@ -0,0 +1,99 @@
+#!/usr/bin/env node
+let fs = require("fs");
+//input
+let inputArr = process.argv.slice(2);
+// console.log(inputArr);
+
+//options
+let optionArr = [];
+let filesArr = [];
+
+//identify -> option
+for (let i = 0; i < inputArr.length; i++) {
+  let firstChar = inputArr[i].charAt(0);
+  if (firstChar == "-") {
+    optionArr.push(inputArr[i]);
+  } else {
+    filesArr.push(inputArr[i]);
+  }
+}
+
+//option-check
+let isBothPresent = optionArr.includes("-b") && optionArr.includes("-n");
+if(isBothPresent==true)
+{
+    console.log("either enter -b or -n");
+    return;
+}
+
+//existence 
+for(let i=0; i<filesArr.length; i++)
+{
+let isPresent = fs.existsSync(filesArr[i]);
+if(isPresent==false) {
+    console.log(`file ${filesArr[i]} is not present`)
+    return;
+}
+}
+//read
+let content = "";
+for(let i=0;i<filesArr.length;i++)
+{
+    let buffer = fs.readFileSync(filesArr[i]);
+    content += buffer + "\r\n";
+}
+// console.log(content);
+
+let contentArr = content.split("\r\n");
+// console.log(contentArr);
+
+// -s
+let isSpresent = optionArr.includes("-s");
+if(isSpresent==true) {
+    for(let i=0; i<contentArr.length; i++)
+    {
+        if(contentArr[i]=="" && contentArr[i-1]=="")
+        {
+            contentArr[i]=null;
+        }
+        else if(contentArr[i]=="" && contentArr[i-1]==null)
+        {
+            contentArr[i]=null;
+        }
+    }
+    let tempArr = [];
+    for(let i=0; i<contentArr.length; i++)
+    {
+        if(contentArr[i]!=null)
+        {
+            tempArr.push(contentArr[i]);
+        }
+    }
+    contentArr = tempArr; 
+}
+// console.log("---------------------------------");
+// console.log(contentArr.join("\n"));
+
+//-n
+let isNpresent = optionArr.includes("-n");
+if(isNpresent==true)
+{
+    for(let i=0; i<contentArr.length; i++)
+    contentArr[i] = `${i+1} ${contentArr[i]}` ;
+
+}
+// console.log(contentArr.join("\n"));
+
+//-b
+let isBpresent = optionArr.includes("-b");
+if(isBpresent==true) {
+    let counter = 1;
+    for(let i=0; i<contentArr.length; i++) {
+        if(contentArr[i]!="")
+        {
+            contentArr[i] = `${counter} ${contentArr[i]}`;
+            counter++;
+        }
+    }
+}
+console.log(contentArr.join("\n"));
